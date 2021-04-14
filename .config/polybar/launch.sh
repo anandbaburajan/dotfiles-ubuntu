edba181c9cb8484988bb09b2f6a4371e9e8c214b
Anand <anandbaburajan@gmail.com>
2021-04-14 10:53:43 +0530

    dual monitor setup

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,5 +2,12 @@
 # Terminate already running bar instances
 killall -q polybar
 
-#launch polybar
-polybar elon
+#launch polybar on multiple monitors
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload elon &
+  done
+else
+  polybar --reload elon &
+fi
